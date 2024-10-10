a75b16025c30918e1584232c3dc7c23b0d7a6100
Dzaky Hadyan Eliyanta <dzakyhadyaneliyanta06@gmail.com>
2024-10-10 08:07:02 +0700

    update percobaan 1

--- SiakadFor07.java
+++ SiakadFor07.java
@@ -3,9 +3,16 @@ public class SiakadFor07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        byte lulus = 0, tidakLulus = 0;
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
+            if (nilai >= 60) {
+                lulus++;
+            }
+            else {
+                tidakLulus++;
+            }
             if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
@@ -15,6 +22,8 @@ public class SiakadFor07 {
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + tidakLulus);
         sc.close();
     }
 }
